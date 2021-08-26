ba6f3295f2541f0181f81f3c173a1eec8135cea6
Lara <75126217+little-evil-genius@users.noreply.github.com>
2021-08-26 17:27:32 +0200

    Update clublist.lang.php

--- inc/languages/deutsch_du/clublist.lang.php
+++ inc/languages/deutsch_du/clublist.lang.php
@@ -1,1 +1,61 @@
+<?php
+$l['clublist'] = 'Clubübersicht';
 
+$l['clublist_add'] = 'Club hinzufügen';
+$l['clublist_add_name'] = 'Clubname';
+$l['clublist_add_name_desc'] = 'Name vom Club';
+$l['clublist_add_time'] = 'Zeit vom Club';
+$l['clublist_add_time_desc'] = 'Montag, 17:00 - 19:00 Uhr';
+$l['clublist_add_conductor'] = 'Leitung';
+$l['clublist_add_position'] = 'Positionsmöglichkeit';
+$l['clublist_add_type'] = 'Kategorie vom Club';
+$l['clublist_add_desc'] = 'Beschreibung vom Club';
+$l['clublist_add_desc_desc'] = 'Eine ausführliche Beschreibung vom Club. Maximale Zeichenanzahl beträgt 5000 Zeichen.';
+$l['clublist_add_send'] = 'Neuen Club hinzufügen';
+
+$l['clublist_edit'] = 'Club bearbeiten';
+$l['clublist_edit_send'] = 'Club editieren';
+
+$l['clublist_conductor'] = 'wird geleitet von:';
+$l['clublist_time'] = 'Zeit:';
+$l['clublist_desc'] = 'Beschreibung';
+$l['clublist_member'] = 'Mitglieder';
+
+$l['clublist_filter'] = 'Filtern nach:';
+$l['clublist_filter_option'] = 'Alle Clubs anzeigen';
+$l['clublist_filter_button'] = 'Clubs filtern';
+
+$l['clublist_join_position'] = 'Position';
+$l['clublist_join_position_desc'] = 'Deine Position innerhalb vom Club';
+$l['clublist_join_position_send'] = 'Mitglied werden';
+
+$l['clublist_memberprofile'] = 'Clubs & Vereine';
+$l['clublist_memberprofile_member'] = 'Mitgliedschaften';
+$l['clublist_memberprofile_conductor'] = 'Leitungen';
+$l['clublist_memberprofile_member_none'] = 'Dieser Charakter ist kein Mitglied in einem Club oder Verein!';
+$l['clublist_memberprofile_conductor_none'] = 'Dieser Charakter leitet keinen Club oder Verein!';
+
+$l['clublist_modcp'] = 'Clubs freischalten';
+$l['clublist_modcp_time'] = 'Zeit:';
+$l['clublist_modcp_conductor'] = 'wird geleitet von:';
+$l['clublist_modcp_createdby'] = 'Eingesendet von:';
+$l['clublist_modcp_declined'] = 'Club ablehnen';
+$l['clublist_modcp_accepted'] = 'Club annehmen';
+
+// WEITERLEITUNGSSEITE
+$l['clublist_join_redirect'] = 'Du bist erfolgreich dem Club beigetreten und wirst nun zurückgeleitet!';
+$l['clublist_leave_redirect'] = 'Du hast erfolgreich den Club verlassen und wirst nun zurückgeleitet!';
+$l['clublist_delete_redirect'] = 'Du hast erfolgreich den Club gelöscht und wirst nun zurückgeleitet!';
+$l['clublist_add_redirect'] = 'Du hast erfolgreich einen neuen Club hinzugefügt und wirst nun zurückgeleitet!';
+$l['clublist_edit_redirect'] = 'Du hast erfolgreich den Club bearbeitet und wirst nun zurückgeleitet!';
+$l['clublist_modcp_declined_redirect'] = 'Du hast erfolgreich den Club abgelehnt und wirst nun zurückgeleitet!';
+$l['clublist_modcp_accepted_redirect'] = 'Du hast erfolgreich den Club angenommen und wirst nun zurückgeleitet!';
+
+// MyALERTS STUFF
+$l['clublist_declined'] = '{1} hat dein Club <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> abgelehnt!';
+$l['myalerts_setting_clublist_declined'] = "Benachrichtigung bei Ablehnung vom Team bei hinzugefügten Clubs?";
+
+$l['clublist_accepted'] = '{1} hat dein Club <strong>"{4}"</strong> in der Kategorie <strong>"{5}"</strong> angenommen!';
+$l['myalerts_setting_clublist_accepted'] = "Benachrichtigung bei Annahme vom Team bei hinzugefügten Clubs?";
+
+?>
